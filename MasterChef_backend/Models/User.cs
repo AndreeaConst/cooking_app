ab7413c26775f79f13ccf44292fd3c5f97711bfd
biancacibotaru <bianca.cibotaru@yahoo.com>
2021-11-05 11:41:12 +0200

    Created User class in backend.

---
+++ MasterChef_backend/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterChef_backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
