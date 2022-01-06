092442e79b64a95a5d60c810c24245584da472d2
biancacibotaru <bianca.cibotaru@yahoo.com>
2022-01-06 18:18:46 +0200

    Bug fixed for displaying the ingredients of the recipes searched by name

--- MasterChef_backend/Controllers/RecipeController.cs
+++ MasterChef_backend/Controllers/RecipeController.cs
@@ -90,8 +90,11 @@ namespace MasterChef_backend.Controllers
                              WHERE [MasterChef].[dbo].[Recipe].Name =@Name";
             string query2 = @"SELECT * FROM [MasterChef].[dbo].[Recipe] 
                              WHERE [MasterChef].[dbo].[Recipe].Name = @Name OR [MasterChef].[dbo].[Recipe].Name LIKE ('%'+' ' + @Name) + '%' OR [MasterChef].[dbo].[Recipe].Name LIKE (@Name +' ' + '%') OR [MasterChef].[dbo].[Recipe].Name LIKE ('%' +' ' + @Name)";
+            string query3 = @"select [MasterChef].[dbo].[Ingredient].Name, [MasterChef].[dbo].[Ingredient].IngredientId, [MasterChef].[dbo].[Recipe_ingredient].Amount from [MasterChef].[dbo].[Ingredient] JOIN [MasterChef].[dbo].[Recipe_ingredient] 
+                ON [MasterChef].[dbo].[Ingredient].IngredientId = [MasterChef].[dbo].[Recipe_ingredient].IngredientId
+                WHERE [MasterChef].[dbo].[Recipe_ingredient].RecipeId =@recipeId";
             string sqlDataSource = _configuration.GetConnectionString("MasterchefAppCon");
-            SqlDataReader reader;
+            SqlDataReader reader, reader2;
             using (SqlConnection connection = new SqlConnection(sqlDataSource))
             {
                 connection.Open();
@@ -102,15 +105,35 @@ namespace MasterChef_backend.Controllers
                     while (reader.Read())
                     {
                         Recipe result = new Recipe();
+                        result.RecipeId = (int)reader["RecipeId"];
                         result.Name = (string)reader["Name"];
                         result.Description = (string)reader["Description"];
                         result.CaloriesNo = (int)reader["CaloriesNo"];
                         result.Image = (string)reader["Image"];
                         result.PreparingTime = (int)reader["PreparingTime"];
                         result.Servings = (int)reader["Servings"];
-                        results.Add(result);
-
-
+                        using (SqlConnection connection2 = new SqlConnection(sqlDataSource))
+                        {
+                            connection2.Open();
+                            List<Ingredient> ingredientList = new List<Ingredient>();
+                            using (SqlCommand myCommand2 = new SqlCommand(query3, connection2))
+                            {
+                                myCommand2.Parameters.AddWithValue("@RecipeId", result.RecipeId);
+                                reader2 = myCommand2.ExecuteReader();
+                                while (reader2.Read())
+                                {
+                                    Ingredient ingredient = new Ingredient();
+                                    ingredient.IngredientName = (string)reader2["Name"];
+                                    ingredient.IngredientId = (int)reader2["IngredientId"];
+                                    ingredient.Amount = (int)reader2["Amount"];
+                                    ingredientList.Add(ingredient);
+                                }
+                            }
+                            reader2.Close();
+                            connection2.Close();
+                            result.ListOfIngredients = ingredientList;
+                            results.Add(result);
+                        }
                     }
 
                     reader.Close();
