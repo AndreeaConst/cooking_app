7a50d015a59d1834e4b4d015f551edc9ec9edb17
biancacibotaru <bianca.cibotaru@yahoo.com>
2022-01-06 18:34:20 +0200

    Bug fixed for displaying ingredients.

--- MasterChef_backend/Controllers/RecipeController.cs
+++ MasterChef_backend/Controllers/RecipeController.cs
@@ -60,7 +60,7 @@ namespace MasterChef_backend.Controllers
                                 while (reader2.Read())
                                 {
                                     Ingredient ingredient = new Ingredient();
-                                    ingredient.IngredientName= (string)reader2["Name"];
+                                    ingredient.IngredientName = (string)reader2["Name"];
                                     ingredient.IngredientId = (int)reader2["IngredientId"];
                                     ingredient.Amount = (int)reader2["Amount"];
                                     ingredientList.Add(ingredient);
@@ -159,17 +159,38 @@ namespace MasterChef_backend.Controllers
                                         while (reader.Read())
                                         {
                                             Recipe result = new Recipe();
+                                            result.RecipeId = (int)reader["RecipeId"];
                                             result.Name = (string)reader["Name"];
                                             result.Description = (string)reader["Description"];
                                             result.CaloriesNo = (int)reader["CaloriesNo"];
                                             result.Image = (string)reader["Image"];
                                             result.PreparingTime = (int)reader["PreparingTime"];
                                             result.Servings = (int)reader["Servings"];
-                                            result.Image = (string)reader["Image"];
-                                            int index = results.FindIndex(r => r.Description.Equals(result.Description));
-                                            if (index == -1)
+                                            using (SqlConnection connection2 = new SqlConnection(sqlDataSource))
                                             {
-                                                results.Add(result);
+                                                connection2.Open();
+                                                List<Ingredient> ingredientList = new List<Ingredient>();
+                                                using (SqlCommand myCommand2 = new SqlCommand(query3, connection2))
+                                                {
+                                                    myCommand2.Parameters.AddWithValue("@RecipeId", result.RecipeId);
+                                                    reader2 = myCommand2.ExecuteReader();
+                                                    while (reader2.Read())
+                                                    {
+                                                        Ingredient ingredient = new Ingredient();
+                                                        ingredient.IngredientName = (string)reader2["Name"];
+                                                        ingredient.IngredientId = (int)reader2["IngredientId"];
+                                                        ingredient.Amount = (int)reader2["Amount"];
+                                                        ingredientList.Add(ingredient);
+                                                    }
+                                                }
+                                                reader2.Close();
+                                                connection2.Close();
+                                                result.ListOfIngredients = ingredientList;
+                                                int index = results.FindIndex(r => r.Description.Equals(result.Description));
+                                                if (index == -1)
+                                                {
+                                                    results.Add(result);
+                                                }
                                             }
 
                                         }
